6a6078cb0ba19a3df5ae606d3f57dc82f5f5c956
NikhilKumarReddyKadiveti <sowjanyakadiveti1037@gmail.com>
2026-02-12 08:23:41 +0530

    Add calculations for minmax and z-score

---
+++ exp 6.R
@@ -0,0 +1,16 @@
+value <- 35
+
+min_val <- 23
+max_val <- 61
+
+minmax <- (value - min_val) / (max_val - min_val)
+minmax
+
+mean_val <- 42
+sd_val <- 12.94
+
+zscore <- (value - mean_val) / sd_val
+zscore
+
+decimal_scaling <- value / 100
+decimal_scaling
